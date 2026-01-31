fb3c22ed2958ae2282920d270061ad9764487c65
XCQ0607 <xcq0607@outlook.com>
2026-01-31 20:14:35 +0800

    2

--- action.js
+++ action.js
@@ -385,8 +385,10 @@ async function launchChrome() {
         '--window-size=1280,720',
         '--no-sandbox',
         '--disable-setuid-sandbox',
+        '--disable-setuid-sandbox',
         `--user-data-dir=${userDataDir}`,
-        '--disable-dev-shm-usage'
+        '--disable-dev-shm-usage',
+        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     ];
 
     const chrome = spawn(CHROME_PATH, args, {
